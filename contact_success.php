97ad7d2371602f0ac683f671cb6f7da7054969b0
parovski <martiiinskopje@yahoo.com>
2019-03-05 16:23:24 +0100

    All files

---
+++ contact_success.php
@@ -0,0 +1,38 @@
+
+<?php 
+// include ("database_connect.php");
+include("header.php"); ?>
+  <link rel="stylesheet" href="css/nav2.css">
+
+<style type="text/css">
+
+
+
+    .contactContent>p{
+        color:black;
+        text-transform: uppercase;
+        text-align:center;
+        font-size:1.5em;
+
+    }
+
+    .contactContent{
+        margin-top:200px;
+    }
+
+</style>
+<div class="container">
+<div class="contactContent">
+	<p>We appreciate you contacting us. One of our colleagues will get back to you shortly. We have received your message and would like to thank you for writing to us. If your inquiry is urgent, please use the telephone number listed below to talk to one of our staff members.</p>
+	<p>+306 XX XX XX XX</p>
+	<p>Kind regards,</p>
+
+	<p>Human Resources</p>
+	<p>Endouble!</p>
+</div>
+</div>
+
+<?php include("footer.php"); ?>
+
+
+
